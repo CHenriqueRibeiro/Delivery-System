372cd1d05f9837e92d33f17b5a0a7a138cd2466b
Henrique <chmr66@gmail.com>
2023-08-29 14:55:00 -0300

    Pastas organizadas

---
+++ Delivery-system/src/App.jsx
@@ -0,0 +1,12 @@
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import Welcome from "./components/Welcome"
+export default function App() {
+    return (
+    <>
+    <Welcome/>
+    </>
+    );
+    }
