0c2fd99fa981797fcb71a8070a6fdd6ce6d2b077
Ezequiel Brilhante <ezequielrb7@gmail.com>
2023-09-06 19:17:40 -0300

    refatoração da lista de carrinho

--- Delivery-system/src/components/Listcart/listcart.jsx
+++ Delivery-system/src/components/Listcart/listcart.jsx
@@ -1,31 +1,58 @@
 // eslint-disable-next-line no-unused-vars
-import * as React from "react";
-import CardCart from "../CardCart/cardCart";
-import "./listcart.css";
+import * as React from 'react';
+import CardCart from '../CardCart/cardCart';
+import './listcart.css';
 
 export default function Listcart() {
+  const listItemsCart = [
+    {
+      id: 1,
+      imageUrl:
+        'https://img1.gratispng.com/20180202/qrq/kisspng-sicilian-pizza-italian-cuisine-pepperoni-tarte-fla-pepperoni-pizza-5a74bcc6c6e634.1018048315175999428147.jpg',
+      flavor: 'Pizza Mista',
+      ingredients: 'Calabresa, Queijo, Mussarela, Orégano',
+      price: 19.99,
+    },
+    {
+      id: 2,
+      imageUrl:
+        'https://img1.gratispng.com/20180202/qrq/kisspng-sicilian-pizza-italian-cuisine-pepperoni-tarte-fla-pepperoni-pizza-5a74bcc6c6e634.1018048315175999428147.jpg',
+      flavor: 'Pizza Calabresa',
+      ingredients: 'Calabresa, Queijo, Mussarela, Orégano',
+      price: 19.99,
+    },
+    {
+      id: 3,
+      imageUrl:
+        'https://img1.gratispng.com/20180202/qrq/kisspng-sicilian-pizza-italian-cuisine-pepperoni-tarte-fla-pepperoni-pizza-5a74bcc6c6e634.1018048315175999428147.jpg',
+      flavor: 'Pizza Mussarela',
+      ingredients: 'Calabresa, Queijo, Mussarela, Orégano',
+      price: 19.99,
+    },
+    {
+      id: 3,
+      imageUrl:
+        'https://img1.gratispng.com/20180202/qrq/kisspng-sicilian-pizza-italian-cuisine-pepperoni-tarte-fla-pepperoni-pizza-5a74bcc6c6e634.1018048315175999428147.jpg',
+      flavor: 'Pizza Mussarela',
+      ingredients: 'Calabresa, Queijo, Mussarela, Orégano',
+      price: 19.99,
+    },
+  ];
+
   return (
     <>
       <div id="cartShoppingList">
         <div className="listCardCart">
           <div className="teste">
-            <CardCart
-              imageUrl="https://img1.gratispng.com/20180202/qrq/kisspng-sicilian-pizza-italian-cuisine-pepperoni-tarte-fla-pepperoni-pizza-5a74bcc6c6e634.1018048315175999428147.jpg"
-              flavor="Pizza Calabresa"
-              ingredients="Calabresa, Queijo, Mussarela, Orégano"
-              price={19.99}
-            /> <CardCart
-            imageUrl="https://img1.gratispng.com/20180202/qrq/kisspng-sicilian-pizza-italian-cuisine-pepperoni-tarte-fla-pepperoni-pizza-5a74bcc6c6e634.1018048315175999428147.jpg"
-            flavor="Pizza Calabresa"
-            ingredients="Calabresa, Queijo, Mussarela, Orégano"
-            price={19.99}
-          /> <CardCart
-          imageUrl="https://img1.gratispng.com/20180202/qrq/kisspng-sicilian-pizza-italian-cuisine-pepperoni-tarte-fla-pepperoni-pizza-5a74bcc6c6e634.1018048315175999428147.jpg"
-          flavor="Pizza Calabresa"
-          ingredients="Calabresa, Queijo, Mussarela, Orégano"
-          price={19.99}
-        />
-            
+            {listItemsCart.map((item, index) => (
+              <CardCart
+                key={index}
+                imageUrl={item.imageUrl}
+                flavor={item.flavor}
+                price={item.price}
+                ingredients={item.ingredients}
+              />
+            ))}
           </div>
         </div>
         <div className="cartSum"> Total : R$ 50,00</div>
