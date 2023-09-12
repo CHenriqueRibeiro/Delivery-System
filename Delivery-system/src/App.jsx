396a6b6132edc62ef8a9f69504fb1001dcd74894
Carlos Henrique <124113375+CHenriqueRibeiro@users.noreply.github.com>
2023-09-12 09:59:53 -0300

    resolvendo pendencia de nomenclatura dos componentes

--- Delivery-system/src/App.jsx
+++ Delivery-system/src/App.jsx
@@ -3,8 +3,8 @@ import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 import "./app.css";
-import Footer from "./components/Footer/footer";
-import Menu from "./components/Menu/menu";
+import Footer from "./components/Footer/Footer";
+import Menu from "./components/Menu/Menu";
 import Header from "./components/Header/Header";
 import Container from "@mui/material/Container";
 import cartContext from "./context/cartContext"
